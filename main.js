702ada85327f636fdbcca487133e86234525db9d
RosaD97 <r960595@gmail.com>
2023-03-21 16:18:29 +0100

    es. completato + bonus

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return {
+            email: []
+        }
+    },
+    beforeMount(){
+        for(let i = 1; i <= 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response)
+                this.email.push(response.data)
+
+            });
+        }
+
+    }
+}).mount('#app');
